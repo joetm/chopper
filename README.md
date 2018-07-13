62e6e2b8e2d44303db345245abdf1c091acbfa84
Jonas <jonaso@web.de>
2018-07-13 14:11:39 +0300

    get to the chopper!

--- README.md
+++ README.md
@@ -11,3 +11,27 @@ G   G E      TT         TT   O   O       TT   H  H E        C    H  H O   O P   
  GGG  EEEE   TT         TT    OOO        TT   H  H EEEE      CCC H  H  OOO  P     P     EEEE R  RR !!! 
 ```
 
+
+Generates screenshots of UI elements.
+
+Usage
+-----
+
+```
+node chopper.js http://example.com div
+```
+
+Argument 1:
+~~~~~~~~~~~
+URL - required
+
+Argument 2:
+~~~~~~~~~~~
+Optional DOM selector. Defaults to 'div'.
+Also supports multiple selectors, e.g. 'header,footer'.
+
+Examples
+--------
+
+See `yarn demo` and `yarn tagesschau`.
+
---
+++ chopper.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+// https://gist.github.com/liangzan/807712/8fb16263cb39e8472d17aea760b6b1492c465af2
+const rmDir = function(dirPath) {
+  try { var files = fs.readdirSync(dirPath); }
+  catch(e) { return; }
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i];
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath);
+      else
+        rmDir(filePath);
+    }
+  fs.rmdirSync(dirPath);
+};
+
+
+// check cmd arguments for url
+if (process.argv[2] === undefined) {
+  console.error('');
+  process.exit();
+}
+const URL = process.argv[2];
+
+// what to extract?
+// defaults
+let DIV_TO_QUERY = 'div';
+if (process.argv[3] !== undefined) {
+  DIV_TO_QUERY = process.argv[3];
+}
+
+console.log(`Generating screenshots for ${DIV_TO_QUERY} from ${URL}`);
+
+
+// clear the img folder first
+const IMGDIR = './img/';
+rmDir(IMGDIR);
+if (!fs.existsSync(IMGDIR)){
+  fs.mkdirSync(IMGDIR);
+}
+
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(URL);
+    // await page.screenshot({path: 'example.png'})
+
+    // console.log(page);
+
+    const elements = await page.$$(DIV_TO_QUERY);
+    // console.log(elements[0])
+
+    let counter = 0;
+    for (const element of elements) {
+      counter++;
+      // console.log(element);
+      const bbox = await element.boundingBox();
+      if (!bbox || !bbox.width || !bbox.height) {
+        console.log(bbox, '- skipping.');
+        continue;
+      }
+      console.log(bbox);
+      const screenshot = await page.screenshot({
+        'path': `./img/test-${counter}.png`,
+        'fullPage': false,
+        'clip': {
+          x: bbox.x,
+          y: bbox.y,
+          width: bbox.width,
+          height: bbox.height,
+        },
+      });
+    }
+
+    await browser.close();
+})();
